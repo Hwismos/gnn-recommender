466160f94bebb1cf0d8f44e780bf454259415ed2
Faith0918 <sinsil0918@gmail.com>
2023-02-07 15:45:03 +0900

    gtn 전용 쉘 스크립트 생성

---
+++ experiment/main/GTN-SIGIR2022/gtn_main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=hgx
+
+#SBATCH --job-name=GTN.out
+#SBATCH -o SLURM.%N.GTN.out
+#SBATCH -e SLURM.%N.GTN.err
+
+#SBATCH --gres=gpu:hgx
+
+hostname
+date
+
+module add ANACONDA/2020.11
+module add CUDA/11.2.2
+CUDA_VISIBLE_DEVICES=2 python -u ./code/run_main.py > ./test/GTN_epochs_1000_gowalla.out
