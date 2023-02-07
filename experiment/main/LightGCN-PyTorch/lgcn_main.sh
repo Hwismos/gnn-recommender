32f010559c404150c5bb3d6f48f9f6c63796569d
Faith0918 <sinsil0918@gmail.com>
2023-02-07 13:39:54 +0900

    쉘 스크립트 수정

--- experiment/main/LightGCN-PyTorch/lgcn_main.sh
+++ experiment/main/LightGCN-PyTorch/lgcn_main.sh
@@ -14,4 +14,4 @@ date
 
 module add ANACONDA/2020.11
 module add CUDA/11.2.2
-CUDA_VISIBLE_DEVICES=2 python -u ./code/main.py > LGCN.out
+CUDA_VISIBLE_DEVICES=2 python -u ./code/main.py > ./test/LGCN_4096_addRandomRatio_both_train_test_gowalla.out
